05af6aca63067fde5f3e44eeed840239a8d90079
Guillermo Lescano <glescano@vates.com>
2023-05-15 01:44:47 -0300

    Primer commit de GuitarLA en NEXTJS

---
+++ app/nosotros.js
@@ -0,0 +1,17 @@
+import Layout from "@/components/layout"
+import Link from "next/link"
+export default function Nosotros() {
+  return ( 
+
+    <Layout
+      title={"Nosotros"}
+      description={"sobre nosotros guitarLA"}
+    >
+
+    
+    <Link href="/">ir a inicio</Link>
+
+    </Layout>
+  )
+}
+  
